e23069538f75804743a16d0f6ea094c15c387bc4
russell-stewart <rstewart16@kentdenver.org>
2016-01-28 11:03:19 -0700

    Add Pun and Review models to index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.use(parser.urlencoded({
   extended: true
 }));
 app.use(parser.json());
-//add models here
+
+var Pun = require('./models/pun.js');
+var Review = require('./models/review.js');
 
 mongoose.connect('mongodb://localhost:27017/pun-database' , function(err){
   if(err) {//If error != undefined
@@ -15,3 +17,15 @@ mongoose.connect('mongodb://localhost:27017/pun-database' , function(err){
     console.log("connected to the database!");
   }
 });
+
+var port = 8001;
+
+var router = express.Router();
+
+app.get('/pun-database' , function(req , res) {
+  res.send({message:"welcome to the api"});
+});
+
+app.use('/pun-database' , router);
+app.listen(port);
+console.log('api active at localhost:' + port);
